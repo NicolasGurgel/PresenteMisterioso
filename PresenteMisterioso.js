906b3e4f8866382d97af2cc5f9b7da551c9c696e
NicolasGurgel <nicolasgurgel777@gmail.com>
2024-09-09 22:26:01 -0300

    comer comer

--- PresenteMisterioso.js
+++ PresenteMisterioso.js
@@ -5,5 +5,5 @@ document.getElementById("titulo").onclick = function() {
 
 // Quando passar o mouse sobre a imagem, exibe um alerta
 document.getElementById("capitao").onmouseover = function() {
-    alert("Chaaana");
+    alert("Filha da puta de Jong");
 }
