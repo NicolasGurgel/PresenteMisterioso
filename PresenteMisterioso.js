c7ba59bb25b8c9a16cdf89a237147eade61b47f6
NicolasGurgel <nicolasgurgel777@gmail.com>
2024-09-10 05:18:25 -0300

    movendo mario

--- PresenteMisterioso.js
+++ PresenteMisterioso.js
@@ -14,3 +14,38 @@ document.getElementById("botao").onmouseover = function() {
     this.style.left = x + "px";
     this.style.top = y + "px";
 }
+
+const mario = document.getElementById("mario");
+
+function jump(){
+    mario.classList.add("jump");
+    setTimeout(function(){
+        mario.classList.remove("jump");
+    }, 500);
+}
+
+function walkR(){
+    let atual = parseInt(window.getComputedStyle(mario).left, 10);
+    mario.classList.remove("faceL");
+    mario.classList.add("faceR");
+    mario.style.left = (atual+50) + "px";
+    
+}
+
+function walkL(){
+    let atual = parseInt(window.getComputedStyle(mario).left, 10);
+    mario.classList.remove("faceR");
+    mario.classList.add("faceL");
+    mario.style.left = (atual-50) + "px";
+    
+}
+
+document.addEventListener("keydown", (event)=>{
+    if(event.code === 'Space'){
+        jump();
+    }else if(event.code === 'KeyD'){
+        walkR();
+    }else if(event.code === 'KeyA'){
+        walkL();
+    }
+});
